02142b36599c33c59acef5c4d8656e402671101e
Jadson Gomes <dev.gomes31@gmail.com>
2023-09-17 01:12:01 -0300

    Peimeiro exercício

---
+++ Ex01.py
@@ -0,0 +1,24 @@
+# Resolução 01
+
+#CRIAR 2 LISTAS (1° COM 5 NOMES) E MAIS (2° COM 5 VALORES EM REAIS)
+lista_clientes=[]
+lista_valores=[]
+
+#USAR OS LAÇOS DE REPETIÇÃO PARA O PREENCHIMENTO DA MESMA FORMA QUE SAIRÁ NO MEN
+print('*'*20,'MENU PRINCIPAL','*'*20)
+print('PARA ENCERRAR O PROGRAMA DIGITE (Sair)')
+
+while True:
+  nome=input('Nome>> ')
+  if nome.lower() == 'sair':
+    break
+  saldo=float(input('Saldo>> '))
+  lista_clientes.append(nome)
+  lista_valores.append(saldo)
+  print('-'*60)
+
+#SAIDA COM A LISTA DE CLIENTES CADASTRADOS
+print('LISTA DE CLIENTES - BANCO NACIONAL')
+print('|  NOME  |  SALDO  |  CONTA  |')
+for conta, (nome, saldo) in enumerate(zip(lista_clientes, lista_valores), start=1):
+  print(' {}   {}   #{}   '.format(nome,saldo,conta))
