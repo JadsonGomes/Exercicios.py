f421d30d1a15d2a76ebac17d6f08bd5c266927f6
JadsonGomes <dev.gomes31@gmail.com>
2025-09-30 23:37:21 -0300

    finalização de exercícios

---
+++ EXERCICIOS/Retomando_os_treinos.py
@@ -0,0 +1,10 @@
+#Fazendo alguns axercícios para relembrar das aulas
+
+numero = float(input('ME DÊ UM VALOR NUMÉRICO:  '))
+
+if numero < 0:
+    print('O número é negativo')
+elif numero > 0:
+    print('O número é positivo!')
+else:
+    print('O número é zero')
